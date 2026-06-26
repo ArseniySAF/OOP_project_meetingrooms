ac848a786badfc3ebdc2f051a3ce7e763c3cb4d3
Muldiyarov Arseniy <muldiyarovars@mail.ru>
2026-06-26 11:07:52 +0300

    Update 20260626061551_create_booking_history.sql

--- migrations/20260626061551_create_booking_history.sql
+++ migrations/20260626061551_create_booking_history.sql
@@ -1,5 +1,17 @@
 -- +goose Up
-SELECT 'up SQL query';
+-- +goose StatementBegin
+CREATE TABLE IF  NOT EXISTS booking_history(
+    id BIGSERIAL PRIMARY KEY,
+    booking_id UUID NOT NULL REFERENCES bookings(id) ON DELETE CASCADE,
+    action VARCHAR(50) NOT NULL,
+    old_status VARCHAR(20),
+    new_status VARCHAR(20),
+    changed_by UUID,
+    changed_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+-- +goose StatementEnd
 
 -- +goose Down
-SELECT 'down SQL query';
+-- +goose StatementBegin
+DROP TABLE IF EXISTS booking_history;
+-- +goose StatementEnd
