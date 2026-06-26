901003b83ba7e65d8bff5c34915b93756d0fc39a
Arseniy Safianchickov <ars070706@mail.ru>
2026-06-26 12:23:51 +0300

    Add db constr

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -1,1 +1,29 @@
 package storage
+
+import (
+	"database/sql"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+type Postgres struct {
+	db *sql.DB
+}
+
+func NewPostgres() (*Postgres, error) {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return nil, err
+	}
+	// defer db.Close()
+
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &Postgres{db: db}, nil
+}
