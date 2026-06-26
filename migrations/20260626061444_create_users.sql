e2673970cccc9b8e30916f8e0221f4a62d44c140
Muldiyarov Arseniy <muldiyarovars@mail.ru>
2026-06-26 11:06:24 +0300

    Update 20260626061444_create_users.sql

--- migrations/20260626061444_create_users.sql
+++ migrations/20260626061444_create_users.sql
@@ -1,5 +1,14 @@
 -- +goose Up
-SELECT 'up SQL query';
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS users(
+    id UUID PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'user')),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+-- +goose StatementEnd
 
 -- +goose Down
-SELECT 'down SQL query';
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
