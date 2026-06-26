8d89c9f2928260d3702b54dcbf278b572e78947f
Muldiyarov Arseniy <muldiyarovars@mail.ru>
2026-06-26 11:06:56 +0300

    Update 20260626061504_create_rooms.sql

--- migrations/20260626061504_create_rooms.sql
+++ migrations/20260626061504_create_rooms.sql
@@ -1,5 +1,18 @@
 -- +goose Up
-SELECT 'up SQL query';
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS rooms(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL UNIQUE,
+    capacity INT NOT NULL CHECK (capacity > 0),
+    floor INT NOT NULL,
+    equipment TEXT[] DEFAULT '{}',
+    is_active BOOLEAN NOT NULL DEFAULT true,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+-- +goose StatementEnd
 
 -- +goose Down
-SELECT 'down SQL query';
+-- +goose StatementBegin
+DROP TABLE IF EXISTS rooms;
+-- +goose StatementEnd
