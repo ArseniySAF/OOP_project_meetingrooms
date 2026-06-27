8131286feef5b6e0dfe8728c46c8aaec33289494
Arseniy Safianchickov <ars070706@mail.ru>
2026-06-27 09:34:56 +0300

    Complete http bookings layer with middleware

--- internal/api/bookings.go
+++ internal/api/bookings.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/google/uuid"
 	openapi_types "github.com/oapi-codegen/runtime/types"
 )
 
@@ -24,7 +25,55 @@ func (hh *HttpHandler) CreateBooking(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: написать Middleware, чтобы извлекать userId(sub) из claims
+	userIdString := r.Header.Get("X-USER-ID")
+
+	userId, err := uuid.Parse(userIdString)
+	if err != nil {
+		errDTO := myerrors.ErrorDTO{
+			Message: err.Error(),
+			Time:    time.Now(),
+		}
+
+		http.Error(w, errDTO.ToString(), http.StatusUnauthorized)
+		return
+	}
+
+	input := models.InputBooking{
+		RoomId:         requestBooking.RoomId,
+		UserId:         userId,
+		Title:          requestBooking.Title,
+		Description:    requestBooking.Description,
+		StartTime:      requestBooking.StartTime,
+		EndTime:        requestBooking.EndTime,
+		AttendeesCount: *requestBooking.AttendeesCount,
+	}
+
+	booking, err := hh.MeetingService.CreateBooking(input)
+	if err != nil {
+		errDTO := myerrors.ErrorDTO{
+			Message: err.Error(),
+			Time:    time.Now(),
+		}
+
+		http.Error(w, errDTO.ToString(), http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.MarshalIndent(booking, "", "	")
+	if err != nil {
+		errDTO := myerrors.ErrorDTO{
+			Message: err.Error(),
+			Time:    time.Now(),
+		}
+
+		http.Error(w, errDTO.ToString(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	if _, err := w.Write(b); err != nil {
+		log.Println(err)
+	}
 }
 
 func (hh *HttpHandler) ListBookings(w http.ResponseWriter, r *http.Request, params ListBookingsParams) {
@@ -64,13 +113,64 @@ func (hh *HttpHandler) GetMyBookings(w http.ResponseWriter, r *http.Request, par
 		Offset: params.Offset,
 	}
 
-	_ = filter
+	userIdString := r.Header.Get("X-USER-ID")
 
-	// TODO: написать Middleware, чтобы извлекать userId(sub) из claims
+	userId, err := uuid.Parse(userIdString)
+	if err != nil {
+		errDTO := myerrors.ErrorDTO{
+			Message: err.Error(),
+			Time:    time.Now(),
+		}
+
+		http.Error(w, errDTO.ToString(), http.StatusUnauthorized)
+		return
+	}
+
+	myBookings, err := hh.MeetingService.GetMyBookings(userId, filter)
+	if err != nil {
+		errDTO := myerrors.ErrorDTO{
+			Message: err.Error(),
+			Time:    time.Now(),
+		}
+
+		http.Error(w, errDTO.ToString(), http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.MarshalIndent(myBookings, "", "	")
+	if err != nil {
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(b); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func (hh *HttpHandler) CancelBooking(w http.ResponseWriter, r *http.Request, id openapi_types.UUID) {
-	// TODO: написать Middleware, чтобы извлекать userId(sub) из claims
+	userIdString := r.Header.Get("X-USER-ID")
 
-	// + какую то проверку на уровне http
+	userId, err := uuid.Parse(userIdString)
+	if err != nil {
+		errDTO := myerrors.ErrorDTO{
+			Message: err.Error(),
+			Time:    time.Now(),
+		}
+
+		http.Error(w, errDTO.ToString(), http.StatusUnauthorized)
+		return
+	}
+
+	if err := hh.MeetingService.DeleteMyBookingById(id, userId); err != nil {
+		errDTO := myerrors.ErrorDTO{
+			Message: err.Error(),
+			Time:    time.Now(),
+		}
+
+		http.Error(w, errDTO.ToString(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
