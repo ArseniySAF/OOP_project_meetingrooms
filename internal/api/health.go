3867ec358ab19fcc4fef818e347dc99e353f8992
Muldiyarov Arseniy <muldiyarovars@mail.ru>
2026-06-27 07:47:32 +0300

    Update health.go

--- internal/api/health.go
+++ internal/api/health.go
@@ -1,1 +1,39 @@
 package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+type HealthCheckDTO struct {
+	Message string
+	Time    time.Time
+}
+
+func (h HealthCheckDTO) ToString() string {
+	b, err := json.MarshalIndent(h, "", "	")
+	if err != nil {
+		panic(err)
+	}
+
+	return string(b)
+}
+
+func (hh *HttpHandler) CheckServerHealth(w http.ResponseWriter, r *http.Request) {
+	response := HealthCheckDTO{
+		Message: "OK",
+		Time:    time.Now(),
+	}
+
+	b, err := json.MarshalIndent(response, "", "	")
+	if err != nil {
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(b); err != nil {
+		log.Fatal(err)
+	}
+}
